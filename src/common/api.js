f9c41fd421a49ea69a77277785c38de86c6a43a9
raman-radius <42926001+raman-radius@users.noreply.github.com>
2019-06-11 17:14:29 +0530

    Update api.js

--- src/common/api.js
+++ src/common/api.js
@@ -10,3 +10,13 @@ module.exports = {
 			}).catch((e)=>console.log(e));
 	}
 }
+
+export default const getSubredditData = (subredditName) =>  {
+	return fetch('https://www.reddit.com/r/'+subredditName+'.json')
+			.then(resp=>resp.json())
+			.then((data)=>{
+				console.log("Resp:",data)
+				result = data.data.children;
+				return result;
+			}).catch((e)=>console.log(e));
+}
